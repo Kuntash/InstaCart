25119e222cd2a4c1ec907aeb78888a9293725c68
Kuntash <kuntashtashi11@gmail.com>
2021-09-30 22:29:17 +0530

    done with styling the accordion faq

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // Making a function which toggles class name when clicked
 const faqQuestions = document.querySelectorAll('.faq__question--question');
 const faqAnswers = document.querySelectorAll('.faq__question--answer');
+const faqLogo = document.querySelectorAll('.plus-logo');
 faqQuestions.forEach((element, index) => {
     element.addEventListener('click', function(){
         faqAnswers[index].classList.toggle('visible');
+        if(faqLogo[index].src.includes("add"))
+        {
+            console.log(faqLogo[index].src);
+            faqLogo[index].setAttribute("src", `images/remove.svg`);
+        }
+        else if(faqLogo[index].src.match("images/remove"))
+        {
+            faqLogo[index].setAttribute("src", "images/add.svg");
+            
+        }
     });
-    
 })
