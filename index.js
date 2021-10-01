4afb573b8913b61c1156c286867e8d9c3a0bb420
Kuntash <kuntashtashi11@gmail.com>
2021-10-01 17:47:30 +0530

    Responsive design for max width 800 done, about to do for 375px

--- index.js
+++ index.js
@@ -20,6 +20,34 @@ faqQuestions.forEach((element, index) => {
     });
 })
 
+//Accordion on lower feature section
+const featureCards2Header = document.querySelectorAll('.feature__cards2--header');
+const featureCards2Sub = document.querySelectorAll('.feature__cards2--sub');
+featureCards2Header.forEach(function(element, index){
+    element.addEventListener('click', function(){
+        console.log(`Clicked`);
+        featureCards2Sub[index].classList.toggle('visible-sub');
+    });
+});
+
+//Accordion on Link
+const linkHeader = document.querySelectorAll('.link__header');
+const linkSub = document.querySelectorAll('.link__sub');
+const arrow = document.querySelectorAll('.arrow');
+linkHeader.forEach(function(element, index) {
+    element.addEventListener('click', function () {
+        if(arrow[index].src.includes("down-arrow"))
+        {
+            arrow[index].setAttribute("src", `images/up-arrow.svg`);
+        }
+        else if(arrow[index].src.includes("up-arrow"))
+        {
+            arrow[index].setAttribute("src", "images/down-arrow.svg");
+        }
+        linkSub[index].classList.toggle('visible-sub');
+    });
+});
+
 //Change nation flag on option
 const langSelector = document.querySelector('#lang__selector');
 const langLogo = document.querySelector('.lang__logo');
